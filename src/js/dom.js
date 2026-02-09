da380769b155182c788b85d9d64b30cc79740e0e
juan <juan.souza125@gmail.com>
2026-02-09 20:49:27 -0300

    melhorias

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,2 +1,4 @@
 export const containerDestinos = document.querySelector(".destinos");
 export const loading = document.querySelector("#loading");
+
+export const botaoMais = document.querySelector("#carregar-mais-paises")
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,31 @@
 import { buscarDestinosAPI } from "./api.js";
-import { containerDestinos, loading } from "./dom.js";
+import { containerDestinos, loading, botaoMais } from "./dom.js";
 import { criarCard } from "./cards.js";
 
+let todosOsPaises = [];
+let limiteAtual = 6;
+const QUANTIDADE = 6;
+
+// função que desenha os cards na tela
+function renderizarPaises() {
+    const paisesParaMostrar = todosOsPaises.slice(
+        limiteAtual - QUANTIDADE,
+        limiteAtual
+    );
+
+    paisesParaMostrar.forEach(pais => {
+        const card = criarCard(pais);
+        containerDestinos.appendChild(card);
+    });
+}
+
+// primeira carga da página
 async function iniciar() {
     loading.style.display = "block";
+
     try {
-        const paises = await buscarDestinosAPI(); //chamada da api e atribuição a constante paises
-
-
-        paises.slice(10, 16).forEach(pais => {
-            const card = criarCard(pais);
-            containerDestinos.appendChild(card);
-        });
-
+        todosOsPaises = await buscarDestinosAPI();
+        renderizarPaises();
     } catch (erro) {
         console.error("Erro ao buscar destinos:", erro);
         loading.textContent = "Erro ao carregar destinos.";
@@ -20,6 +33,19 @@ async function iniciar() {
         loading.style.display = "none";
     }
 }
+
+// clique no botão "Carregar mais"
+botaoMais.addEventListener("click", async () => {
+    loading.style.display = "block";
+    limiteAtual += QUANTIDADE;
+    renderizarPaises();
+    loading.style.display = "none";
+
+    if (limiteAtual >= todosOsPaises.length) {
+        botaoMais.disabled = true;
+        botaoMais.textContent = "Não há mais países";
+    }
+});
 
 iniciar();
 
